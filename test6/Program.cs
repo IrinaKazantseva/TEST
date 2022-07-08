f3287888503226da938e048a8620a304a8f780cb
Irina <skorpya@yandex.ru>
2022-07-08 22:26:40 +0300

    numbers

--- test6/Program.cs
+++ test6/Program.cs
@@ -1,11 +1,11 @@
 ﻿Console.WriteLine("Введите число ");
 int N = Convert.ToInt32(Console.ReadLine());
 int count = 0;
-int number = -1 * N;
+int number = -1 * N; // int count = -N; 
 
-while (count  < 2 * N + 1)
+while (count  < 2 * N + 1) // count <= N;
 {
-Console.WriteLine(number );
+Console.WriteLine(number ); // Console.WriteLine(count);
 number = number + 1;
-count = count + 1;
+count = count + 1; // count++;
 }
