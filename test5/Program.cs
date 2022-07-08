e6b604486d38c0eda3f9200a6af5c084a9fd7304
Irina <skorpya@yandex.ru>
2022-07-08 21:32:43 +0300

    test5

---
+++ test5/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Write("Введите номер дня недели: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA == 1)
+{
+Console.WriteLine("понедельник");
+}
+
+if (numberA == 2)
+{
+Console.WriteLine("вторник");
+}
+
+if (numberA == 3)
+{
+Console.WriteLine("среда");
+}
+
+if (numberA == 4)
+{
+Console.WriteLine("четверг");
+}
+
+if (numberA == 5)
+{
+Console.WriteLine("пятница");
+}
+
+if (numberA == 6)
+{
+Console.WriteLine("суббота");
+}
+
+if (numberA == 7)
+{
+Console.WriteLine("воскресенье");
+}
