00a01c0cf0ec7dd2e0c685d7c2a87575bb150179
Irina <skorpya@yandex.ru>
2022-07-09 16:55:40 +0300

    All_Even_Numbers

---
+++ HomeWork/Task_8_All_Even_Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+if (N < 1)
+{
+    Console.WriteLine("Введенное число не удовлетворяет условию задачи");
+}
+else
+{
+   if (N == 1)
+   {
+    Console.WriteLine("четные числа отсутствуют");
+   }
+   else
+   {
+    while (count <= N)
+    {
+        if (count % 2 == 0)
+        {
+        Console.Write(count);
+        Console.Write(" ");
+        }
+    count++;
+    }
+   }
+}
