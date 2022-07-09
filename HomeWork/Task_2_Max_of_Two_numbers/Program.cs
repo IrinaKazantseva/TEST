1e6981308611169dafc43295f44f950460d1f276
Irina <skorpya@yandex.ru>
2022-07-09 15:46:37 +0300

    Max_of_Two_Numbers

---
+++ HomeWork/Task_2_Max_of_Two_numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число 1: ");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int Number2 = Convert.ToInt32(Console.ReadLine());
+
+if (Number1 == Number2)
+{
+    Console.WriteLine("Невозможно определить максимум, числа совпадают");
+}
+else if (Number1 > Number2)
+{
+    Console.Write("max = ");
+    Console.WriteLine(Number1);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(Number2);
+}
