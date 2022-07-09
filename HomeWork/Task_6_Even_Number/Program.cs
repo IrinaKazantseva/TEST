ca6c6c0bc556ea106ac8b608b9b6bcb51936474f
Irina <skorpya@yandex.ru>
2022-07-09 15:53:49 +0300

    Even_Number

---
+++ HomeWork/Task_6_Even_Number/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N % 2 == 0)
+{
+    Console.Write(N);
+    Console.WriteLine(" - четное число");
+}
+else
+{
+    Console.Write(N);
+    Console.WriteLine(" - нечетное число");
+}
