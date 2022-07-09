07f768a7f499aed44f27ac8377343fcabd111b13
Irina <skorpya@yandex.ru>
2022-07-09 16:15:00 +0300

    Max_of_Three_Numbers

---
+++ HomeWork/Task_4_Max_of_Three_numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите число 1: ");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int Number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 3: ");
+int Number3 = Convert.ToInt32(Console.ReadLine());
+
+
+if (Number2 > Number1)
+{
+    if (Number2 > Number3)
+    {
+        Console.Write("Максимальное число - ");
+        Console.WriteLine(Number2);
+    }
+    else
+    {
+        Console.Write("Максимальное число - ");
+        Console.WriteLine(Number3);
+    }
+}
+else
+{
+    if (Number1 > Number3)
+    {
+        Console.Write("Максимальное число - ");
+        Console.WriteLine(Number1);
+    }
+    else
+    {
+        Console.Write("Максимальное число - ");
+        Console.WriteLine(Number3);
+    }
+}
