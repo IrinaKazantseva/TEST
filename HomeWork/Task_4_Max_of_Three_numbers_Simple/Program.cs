3952fde24a51d19a5e793212045388e98ca0b942
Irina <skorpya@yandex.ru>
2022-07-10 12:27:28 +0300

    task4_simple

---
+++ HomeWork/Task_4_Max_of_Three_numbers_Simple/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число 1: ");
+int Number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int Number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 3: ");
+int Number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = Number1;
+
+if (Number2 > max) 
+{
+    max = Number2;
+}
+if (Number3 > max) 
+{
+    max = Number3;
+}
+Console.Write("Максимальное число = ");
+Console.WriteLine(max);
