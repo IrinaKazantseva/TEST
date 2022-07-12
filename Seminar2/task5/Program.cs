ca1b3f3464f131ed6ae66b8a8d5608613e1d2f67
Irina <skorpya@yandex.ru>
2022-07-12 22:13:25 +0300

    seminar2

---
+++ Seminar2/task5/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число 1: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int square = numberB * numberB;
+int square1 = numberA * numberA;
+
+if (numberA == square || numberB == square1)
+{
+Console.WriteLine("первое число - квадрат второго или второе число - квадрат первого");
+}
+else
+{
+Console.WriteLine("первое число не квадрат второго или второе число не квадрат первого");
+}
