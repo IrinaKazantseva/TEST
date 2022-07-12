5d6d1200f872e401faad72a448a6b8da7d5bd143
Irina <skorpya@yandex.ru>
2022-07-12 20:29:34 +0300

    seminar2

--- Seminar2/task1/Program.cs
+++ Seminar2/task1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = new Random().Next(10,100); // 10-99 включительно
+
+Console.WriteLine($"Наше случайное число = {number}");
+
+int secondDigit = number % 10; // остаток от деления на 10
+int firstDigit = number / 10; 
+
+Console.WriteLine($"Первая цифра = {firstDigit}, вторая цифра = {secondDigit}");
+
+if (firstDigit > secondDigit)
+{ 
+Console.WriteLine($"Первая цифра числа {number} больше. Это {firstDigit}");
+}
+else
+{ 
+ Console.WriteLine($"Вторая цифра числа {number} больше. Это {secondDigit}");   
+}
