56f388248ec5245f6b2527d5267d6776b6f59cec
Irina <skorpya@yandex.ru>
2022-07-13 00:48:43 +0300

    Third_digit_of_number_negative_included

---
+++ HomeWork2/Task_13_Third_Digit_Of_Number_Negative_Included/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите число: ");
+int OrigNumber = Convert.ToInt32(Console.ReadLine());
+int Number = OrigNumber;
+
+if (OrigNumber < 100 && OrigNumber > -100 )
+{
+   Console.WriteLine("Ошибка: в данном числе нет третьей цифры");
+}
+else
+{
+    if (OrigNumber > 0)
+    {
+    while (Number > 1000)
+    {
+        Number = Number / 10;
+    }
+    Console.WriteLine($"Третья цифра числа {OrigNumber} = {Number % 10}");
+    }
+    else
+    {
+        while (-Number > 1000)
+    {
+        Number = Number / 10;
+    }
+    Console.WriteLine($"Третья цифра числа {OrigNumber} = {-Number % 10}");
+    }
+}
