2662b7d1d899c74649874e542768f46d9fcd8f84
Irina <skorpya@yandex.ru>
2022-07-12 21:20:52 +0300

    seminar2

--- Seminar2/task2/Program.cs
+++ Seminar2/task2/Program.cs
@@ -5,5 +5,5 @@ Console.WriteLine($"Наше случайное число = {number}");
 int thirdDigit = number % 10; // остаток от деления на 10
 int firstDigit = number / 100; 
 
-int N = Convert.ToInt32(firstDigit + "" + thirdDigit);
+int N = Convert.ToInt32(firstDigit + "" + thirdDigit); // int result = digit1*10 + digit3;
 Console.WriteLine(N);
---
+++ Seminar2/task3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число 1: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+
+if (numberB % numberA == 0)
+{
+Console.WriteLine("число 2 кратно числу 1");
+}
+else
+{
+Console.Write("число 2 не кратно числу 1, остаток = ");
+Console.WriteLine(numberB % numberA);  
+}
