2a15bc7bac86e40cbb1daa88cab7dc827c6ccc2a
Irina <skorpya@yandex.ru>
2022-07-12 21:27:09 +0300

    seminar2

--- Seminar2/task3/Program.cs
+++ Seminar2/task3/Program.cs
@@ -1,15 +1,16 @@
 ﻿Console.Write("Введите число 1: ");
-int numberA = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число 2: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
 
 
 if (numberB % numberA == 0)
 {
-Console.WriteLine("число 2 кратно числу 1");
+Console.WriteLine("число 1 кратно числу 2");
 }
 else
 {
-Console.Write("число 2 не кратно числу 1, остаток = ");
-Console.WriteLine(numberB % numberA);  
+//Console.Write("число 1 не кратно числу 2, остаток = ");
+//Console.WriteLine(numberB % numberA);
+Console.WriteLine($"Число 1 не кратно числу 2. Остаток {numberB % numberA}");
 }
