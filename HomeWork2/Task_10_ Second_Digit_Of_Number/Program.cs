557b8b82d874303047d973e7fab1106c3169f112
Irina <skorpya@yandex.ru>
2022-07-12 23:39:26 +0300

    second_digit_of_number_above_zero

---
+++ HomeWork2/Task_10_ Second_Digit_Of_Number/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите трёхзначное число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number > 99 && Number < 1000)
+{
+    int SecondDigit = (Number / 10) % 10;
+    Console.WriteLine($"Вторая цифра числа {Number} = {SecondDigit}");
+}
+else
+    Console.WriteLine("Ошибка: введено не трехзначное число");
