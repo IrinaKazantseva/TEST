29918b5137d5fc4e9a4426814bdfbcbe09bd1f95
Irina <skorpya@yandex.ru>
2022-07-12 23:23:34 +0300

    weekdays_through_if

---
+++ HomeWork2/Task_15_Weekend_2_Way/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите порядковый номер дня недели: ");
+int WeekDayNumber = Convert.ToInt32(Console.ReadLine());
+
+if (WeekDayNumber > 0 && WeekDayNumber <= 7)
+{
+    if (WeekDayNumber > 0 && WeekDayNumber <= 5)
+    {
+        Console.WriteLine("Сегодня рабочий день");
+    }
+    else
+    Console.WriteLine("Сегодня выходной день");
+}
+else
+Console.WriteLine("данные введены некорректно");
