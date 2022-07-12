f92db2103a90ee206396aed69ccd98086637d632
Irina <skorpya@yandex.ru>
2022-07-13 00:35:12 +0300

    Third_digit_of_number

---
+++ HomeWork2/Task_13_Third_Digit_Of_Number/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int OrigNumber = Convert.ToInt32(Console.ReadLine());
+int Number = OrigNumber;
+
+if (OrigNumber <100)
+{
+   Console.WriteLine("Ошибка: в данном числе нет третьей цифры");
+}
+else
+{
+    while (Number > 1000)
+    {
+        Number = Number / 10;
+    }
+    Console.WriteLine($"Третья цифра числа {OrigNumber} = {Number % 10}");
+}
