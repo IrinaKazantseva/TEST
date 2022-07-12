9fde9de9d0a3fe809c4ed28677d435a2c7b0a7d8
Irina <skorpya@yandex.ru>
2022-07-12 21:39:51 +0300

    seminar2

---
+++ Seminar2/task4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 7 == 0 && number % 23 == 0)
+{
+Console.WriteLine($"Число {number} кратно и 7 и 23 одновременно");
+}
+else
+{
+Console.WriteLine($"Число {number} не кратно и 7 и 23 одновременно");
+}
