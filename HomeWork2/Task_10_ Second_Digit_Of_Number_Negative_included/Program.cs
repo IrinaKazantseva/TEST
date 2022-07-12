25ff7cc0e2c0c58c302af6cb6c09a62e60b07ee5
Irina <skorpya@yandex.ru>
2022-07-12 23:48:09 +0300

    second_digit_of_number_negative_numbers_included

---
+++ HomeWork2/Task_10_ Second_Digit_Of_Number_Negative_included/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите трёхзначное число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number > 99 && Number < 1000)
+{
+    int SecondDigit = (Number / 10) % 10;
+    Console.WriteLine($"Вторая цифра числа {Number} = {SecondDigit}");
+}
+else if (Number > -1000 && Number < -99)
+{
+    int SecondDigit = (-Number / 10) % 10;
+    Console.WriteLine($"Вторая цифра числа {Number} = {SecondDigit}");
+}
+else
+    Console.WriteLine("Ошибка: введено не трехзначное число");
