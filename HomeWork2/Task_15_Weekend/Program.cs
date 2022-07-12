9300b49c7374a100fb9cb36b4262ced333288171
Irina <skorpya@yandex.ru>
2022-07-12 23:14:10 +0300

    weekdays_through_switch

---
+++ HomeWork2/Task_15_Weekend/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите порядковый номер дня недели: ");
+int WeekDayNumber = Convert.ToInt32(Console.ReadLine());
+
+switch (WeekDayNumber)
+{
+    case 1:
+        Console.WriteLine("Рабочий день");
+        break;
+    case 2:
+        Console.WriteLine("Рабочий день");
+        break;
+    case 3:
+        Console.WriteLine("Рабочий день");
+        break;
+    case 4:
+        Console.WriteLine("Рабочий день");
+        break;
+    case 5:
+        Console.WriteLine("Рабочий день");
+        break;
+    case 6:
+        Console.WriteLine("Выходной день");
+        break;
+    case 7:
+        Console.WriteLine("Выходной день");
+        break;
+    default :
+        Console.WriteLine("Введены неверные данные");
+        break;
+}
