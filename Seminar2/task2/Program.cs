a4220962199a05dbd9a09db2de5665ee21dd091e
Irina <skorpya@yandex.ru>
2022-07-12 20:47:43 +0300

    seminar2

---
+++ Seminar2/task2/Program.cs
@@ -0,0 +1,9 @@
+﻿int number = new Random().Next(100,1000); // 100-999 включительно
+
+Console.WriteLine($"Наше случайное число = {number}");
+
+int thirdDigit = number % 10; // остаток от деления на 10
+int firstDigit = number / 100; 
+
+int N = Convert.ToInt32(firstDigit + "" + thirdDigit);
+Console.WriteLine(N);
