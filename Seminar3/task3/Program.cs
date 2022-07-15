aa80922a06273f6b492893189439d4c406305764
Irina <skorpya@yandex.ru>
2022-07-15 20:58:04 +0300

    3_Seminar

---
+++ Seminar3/task3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите X1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите X2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите Y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите Y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+int[] array1 = new int [] {x1, y1};
+int[] array2 = new int [] {x2, y2};
+
+double distance = Math.Sqrt(Math.Pow(array2[0] - array1[0], 2) + Math.Pow(array2[1] - array1[1], 2));
+
+Console.WriteLine(distance);
