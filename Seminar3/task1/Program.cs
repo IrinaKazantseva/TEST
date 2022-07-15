7639fa6d23c543f3f1a1a6e9d7be86ca3441656c
Irina <skorpya@yandex.ru>
2022-07-15 20:22:23 +0300

    3_Seminar

---
+++ Seminar3/task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x == 0 || y == 0) 
+{
+Console.WriteLine("x и/или y = 0. Невозможно определить четверть");
+}
+else
+{
+    if (x > 0 && y > 0) Console.WriteLine("Точка находится в первой четверти");
+    if (x < 0 && y > 0) Console.WriteLine("Точка находится во второй четверти");
+    if (x < 0 && y < 0) Console.WriteLine("Точка находится в третьей четверти");
+    if (x > 0 && y < 0) Console.WriteLine("Точка находится в четвертой четверти");
+}
