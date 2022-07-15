c88b43759e9afd6231dbd6b6c912b653ccc93fc8
Irina <skorpya@yandex.ru>
2022-07-15 21:55:51 +0300

    HomeWork3

--- HomeWork3/task19/Program.cs
+++ HomeWork3/task19/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int digit1 = number/10000;
+int digit5 = number%10;
+int digit2 = (number/1000)%10;
+int digit4 = (number/10)%10;
+
+if (number < 10000 || number > 99999)
+{
+Console.WriteLine("Данные введены неверно, укажите пятизначное число");
+}
+else if (digit1 == digit5 && digit2 == digit4)
+{
+    Console.WriteLine("Введенное число является палиндромом");
+}
+else
+{
+    Console.WriteLine("Введенное число не является палиндромом");
+}
