ca2b8616f23960e6f38908a75926beac90f898d2
Irina <skorpya@yandex.ru>
2022-07-15 20:36:40 +0300

    3_Seminar

---
+++ Seminar3/task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите номер четверти: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1 || number > 4) 
+{
+Console.WriteLine("Данные введены неверно, укажите диапазон от 1 до 4");
+}
+else
+{
+    if (number == 1) Console.WriteLine("x > 0 и y > 0");
+    if (number == 2) Console.WriteLine("x < 0 и y > 0");
+    if (number == 3) Console.WriteLine("x < 0 и y < 0");
+    if (number == 4) Console.WriteLine("x > 0 и y < 0");
+}
